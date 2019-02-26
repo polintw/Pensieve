1ea787a6e18b495e035a94a8bd7c0b85096271a4
polintw <21116945+polintw@users.noreply.github.com>
2019-02-26 17:55:05 +0800

    refactor: inspire POST api update to new structure & fix Sequelize bugs.

--- src/inspire/plain.js
+++ src/inspire/plain.js
@@ -4,79 +4,51 @@ const fs = require('fs');
 const path = require("path");
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-const {verify_key} = require('../../../config/jwt.js');
-const {connection_key} = require('../../../config/database.js');
+const {verify_key} = require('../../config/jwt.js');
+const winston = require('../../config/winston.js');
+const _DB_inspired = require('../../db/models/index').inspired;
+const {_res_success} = require('../utils/resHandler.js');
+const {
+  _handle_ErrCatched,
+  forbbidenError,
+  internalError,
+  authorizedError,
+  notFoundError
+} = require('../utils/reserrHandler.js');
 
-const database = mysql.createPool(connection_key);
+/*
+connection.query('DELETE FROM inspired WHERE id_mark = ? AND id_user = ?', [markId, userId], function(err, result, fields) {
+  if (err) {_handler_err_Internal(err, res);reject(err);}
+  console.log('database connection: success.')
+  resolve()
+})
+*/
 
+function _handle_inspire_plain_POST(req, res){
+  new Promise((resolve, reject)=>{
+    const reqToken = req.body.token || req.headers['token'] || req.query.token;
+    const jwtVerified = jwt.verify(reqToken, verify_key);
+    if (!jwtVerified) throw new internalError(jwtVerified, 131)
 
-function _handler_err_BadReq(err, res){
-  let resData = {};
-  resData['error'] = 1;
-  resData['message'] = 'Error Occured: bad database query';
-  res.status(400).json(resData);
- }
+    let userId = jwtVerified.user_Id;
+    const markId = parseInt(req.query.markId);
 
-function _handler_err_Unauthorized(err, res){
-  let resData = {};
-  resData['error'] = 1;
-  resData['message'] = "Token is invalid";
-  res.status(401).json(resData);
-}
-
-function _handler_err_Internal(err, res){
-  let resData = {};
-  resData['error'] = 1;
-  resData['message'] = 'Error Occured: Internal Server Error';
-  res.status(500).json(resData);
-}
-
-function _handle_actionInspired(req, res){
-  jwt.verify(req.headers['token'], verify_key, function(err, payload) {
-    if (err) {
-      _handler_err_Unauthorized(err, res)
-    } else {
-      let userId = payload.user_Id;
-      database.getConnection(function(err, connection){
-        if (err) {
-          _handler_err_Internal(err, res);
-          console.log("error occured when getConnection in actionInspired handler.")
-        }else{
-          new Promise((resolve, reject)=>{
-            console.log('action post: inspired aim to '+req.query.aim);
-            const markId = parseInt(req.body.markKey);
-            if(req.query.aim == "new"){
-                connection.query('INSERT INTO inspired SET ?', {id_mark: markId, id_user: userId}, function(err, result, fields) {
-                    if (err) {_handler_err_Internal(err, res);reject(err);}
-                    console.log('database connection: success.')
-                    resolve()
-                })
-            }else{
-              connection.query('DELETE FROM inspired WHERE id_mark = ? AND id_user = ?', [markId, userId], function(err, result, fields) {
-                if (err) {_handler_err_Internal(err, res);reject(err);}
-                console.log('database connection: success.')
-                resolve()
-              })
-            }
-          }).then(()=>{
-            let resData = {};
-            resData['error'] = 0;
-            resData['message'] = 'post req completed!';
-            res.status(200).json(resData);
-            connection.release();
-          }).catch((err)=>{
-            console.log("error occured in actionInspired promise: "+err)
-            connection.release();
-          });
-        }
-      })
+    _DB_inspired.create({id_mark: markId, id_user: userId}).then(function(createdInspire) {
+      resolve()
+    })
+  }).then(()=>{
+    let sendingData ={
+      temp:{}
     }
-  })
+    _res_success(res, sendingData, "POST: inspire/ , complete.");
+  }).catch((error)=>{
+    _handle_ErrCatched(error, req, res);
+  });
 }
 
 execute.post('/', function(req, res){
-  console.log('request: POST, units/broad');
-  _handle_actionInspired(req, res);
+  if(process.env.NODE_ENV == 'development') winston.verbose('POST: inspire/ ');
+  _handle_inspire_plain_POST(req, res);
 })
 
 module.exports = execute;
