c22111f53bae1064dcb6155cf1236b5c439bde91
polintw <21116945+polintw@users.noreply.github.com>
2019-03-26 19:14:41 +0800

    tune: implement new 'around' icon, and finish the Terrace view.

--- app/Terrace/component/Screen.jsx
+++ app/Terrace/component/Screen.jsx
@@ -6,6 +6,18 @@ import {
 } from 'react-router-dom';
 import {connect} from "react-redux";
 import cxBind from 'classnames/bind';
+import SvgAround from '../../Component/Svg/SvgAround.jsx';
+
+const styleMiddle = {
+  base: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    boxSizing: 'border-box',
+  }
+}
 
 class Screen extends React.Component {
   constructor(props){
@@ -17,17 +29,9 @@ class Screen extends React.Component {
     this._handleClick_nav_expand = this._handleClick_nav_expand.bind(this);
     this._handleClick_selfClose = this._handleClick_selfClose.bind(this);
     this.style={
-      terraceCom_Screen_: {
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        boxSizing: 'border-box',
-      },
       terraceCom_Screen_plane_: {
         width: '100%',
-        height: '58%',
+        height: '53.5%',
         position: 'absolute',
         top: '0',
         right: '0',
@@ -35,52 +39,56 @@ class Screen extends React.Component {
         boxShadow: 'inset 0px -0.24rem 0.3rem -0.28rem',
         backgroundColor: '#FAFAFA'
       },
+      terraceCom_Screen_floor_: {
+        width: '100%',
+        height: '46.5%',
+        position: 'absolute',
+        bottom: '0',
+        right: '0',
+        boxSizing: 'border-box',
+        boxShadow: '0 0.24rem 0.5rem -0.28rem inset',
+        backgroundColor: '#FFFFFF'
+      },
       terraceCom_Screen_account_: {
         maxWidth: '40%',
         position: 'absolute',
-        top: '27%',
-        left: '22%',
+        top: '51.5%',
+        left: '30%',
+        transform: 'translate(-36%,-50%)',
         boxSizing: 'border-box'
       },
       terraceCom_Screen_account_name: {
         display: 'inline-block',
         position: 'relative',
         boxSizing: 'border-box',
-        float: 'right',
-        fontSize: "3.2rem",
-        letterSpacing: '0.35rem',
+        fontSize: "3.9rem",
+        letterSpacing: '0.3rem',
         fontWeight: '400',
-        color: '#222222'
+        color: '#000000'
       },
       terraceCom_Screen_options_: {
         position: 'absolute',
-        top: '30%',
-        right: '34%',
+        top: '48%',
+        right: '24%',
         boxSizing: 'border-box'
       },
       terraceCom_Screen_options_expand: {
-        display: 'inline-block',
+        display: 'inline',
         position: 'relative',
         boxSizing: 'border-box',
-        fontWeight: '300',
+        fontWeight: '400',
         fontSize: "2.4rem",
         letterSpacing: '0.15rem',
         color: '#ff7a5f',
         cursor: 'pointer'
       },
       terraceCom_Screen_return_: {
-        width: '9%',
-        height: '9%',
-        minHeight: '56px',
+        width: '6%',
+        height: '11%',
         position: 'absolute',
-        bottom: '10%',
-        left: '20%',
-        boxSizing: 'border-box'
-      },
-      terraceCom_Screen_return_svg: {
-        width: '50%',
-        maxHeight: '100%',
-        cursor: 'pointer'
+        top: '37%',
+        right: '29%',
+        boxSizing: 'border-box',
       },
     }
   }
@@ -114,16 +122,16 @@ class Screen extends React.Component {
       ):(
         <div
           ref={(element)=>{this.terrace_pagenav=element;}}
-          style={this.style.terraceCom_Screen_}
+          style={styleMiddle.styleMiddle}
           onWheel={this._handleMouse_terraceFrame}>
           <div
             style={this.style.terraceCom_Screen_plane_}>
             <div style={this.style.terraceCom_Screen_account_}>
-              <span style={this.style.terraceCom_Screen_account_name}>{this.props.userInfo.firstName}</span>
-              <span style={this.style.terraceCom_Screen_account_name}>{this.props.userInfo.lastName}</span>
+              <span style={this.style.terraceCom_Screen_account_name}>{this.props.userInfo.firstName+" "+this.props.userInfo.lastName}</span>
             </div>
             <div
               style={this.style.terraceCom_Screen_options_}>
+              <div style={{display: 'inline',position: 'relative',top:'-0.24rem',marginRight:'8%',borderLeft: 'solid 0.2rem #e6e6e6'}}></div>
               <div
                 style={this.style.terraceCom_Screen_options_expand}
                 onClick={this._handleClick_nav_expand}>
@@ -131,15 +139,12 @@ class Screen extends React.Component {
               </div>
             </div>
           </div>
-          <div>
+          <div
+            style={this.style.terraceCom_Screen_floor_}>
             <div
               style={this.style.terraceCom_Screen_return_}
               onClick={this._handleClick_selfClose}>
-              <svg
-                style={this.style.terraceCom_Screen_return_svg}>
-                <text x="50%" y="50%" textAnchor="middle" stroke="#999999" strokeWidth="1.2px" fontSize='3vh'>{" x "}</text>
-                <circle r="2vh" cx="50%" cy="50%" stroke='#999999' fill="transparent"/>
-              </svg>
+              <SvgAround/>
             </div>
           </div>
         </div>
