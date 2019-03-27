6de06386bb6ccdf99c7cc2e61ab532e371fa5d87
polintw <21116945+polintw@users.noreply.github.com>
2019-03-27 17:56:19 +0800

    tune: finish icon around implementation, & layout of Self.

--- app/Self/component/NavSelf.jsx
+++ app/Self/component/NavSelf.jsx
@@ -6,9 +6,50 @@ import {
 } from 'react-router-dom';
 import {connect} from "react-redux";
 import cxBind from 'classnames/bind';
+import SvgAroundLineBlank from '../../Component/Svg/SvgAroundLineBlank.jsx';
 
 const commonStyle={
-
+  boxAccount: {
+    display:'inline-block',
+    width: '70%',
+    height: '42%',
+    position: 'absolute',
+    top: '27%',
+    left: '27%',
+    boxSizing: 'border-box',
+    paddingLeft: '4%',
+    borderLeft: 'solid 0.16rem #e6e6e6'
+  },
+  boxAroundIcon: {
+    display: 'inline-block',
+    width: '20%',
+    height: '78%',
+    position: 'absolute',
+    left: '5%',
+    boxSizing: 'border-box',
+    padding: '2.4% 0 1%',
+    cursor: 'pointer'
+  },
+  boxAroundIconLine: {
+    display: 'inline-block',
+    width: '46%',
+    height: '21%',
+    position: 'absolute',
+    bottom: '1%',
+    left: '9%',
+    boxSizing: 'border-box',
+  },
+  spanAccountName: {
+    display: 'inline-block',
+    position: 'absolute',
+    bottom: '2%',
+    boxSizing: 'border-box',
+    whiteSpace: 'nowrap',
+    fontSize: '1.54rem',
+    fontWeight: '400',
+    letterSpacing: '0.18rem',
+    cursor: 'pointer'
+  }
 }
 
 class NavSelf extends React.Component {
@@ -24,38 +65,10 @@ class NavSelf extends React.Component {
         width: '100%',
         height: '100%',
         position: 'relative',
-        boxSizing: 'border-box',
-        padding: '1vh 0px 0.64vh'
-      },
-      Com_NavSelf_AccountBack_: {
-        display:'inline-block',
-        position: 'relative',
-        boxSizing: 'border-box',
-        paddingLeft: '3%',
-        borderLeft: 'solid 0.18rem #E6E6E6',
-        float: 'left',
-        textAlign: 'right',
-        color: '#E6E6E6',
-        cursor: 'pointer'
-      },
-      Com_NavSelf_Scape: {
-        display: 'inline-block',
-        width: '27%',
-        height: '81%',
-        position: 'relative',
-        boxSizing: 'border-box',
-        float: 'left',
-        cursor: 'pointer'
+        boxSizing: 'border-box'
       },
       Com_NavSelf_AccountName: {
-        display: 'inline',
-        position: 'relative',
-        boxSizing: 'border-box',
-        whiteSpace: 'nowrap',
-        fontSize: '1.4rem',
-        fontWeight: '400',
-        letterSpacing: '0.16rem',
-        color: '#E6E6E6',
+        color: '#757575',
       }
     }
   }
@@ -78,17 +91,33 @@ class NavSelf extends React.Component {
       <div
         style={this.style.Com_NavSelf_}>
         <div
-          style={this.style.Com_NavSelf_Scape}
+          style={commonStyle.boxAroundIcon}
           onClick={this._handleClick_selfClose}>
-
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.54 13.9"
+            style={{
+              maxWidth: '100%',
+              maxHeight: '100%'
+            }}>
+            <defs><style>{".cls-1-aroundDual{fill:#4085a0;}.cls-2-aroundDual{fill:#e6e6e6;}"}</style></defs>
+            <g id="圖層_2" data-name="圖層 2">
+              <g id="圖層_2-2" data-name="圖層 2">
+                <circle class="cls-1-aroundDual" cx="2.46" cy="11.44" r="2.46"/>
+                <circle class="cls-2-aroundDual" cx="13.25" cy="4.3" r="4.3"/>
+              </g>
+            </g>
+          </svg>
         </div>
         <div
-          style={this.style.Com_NavSelf_AccountBack_}
+          style={commonStyle.boxAccount}
           onClick={this._handleClick_selfCover}>
-          <span style={this.style.Com_NavSelf_AccountName}>{this.props.userInfo.firstName + " " + this.props.userInfo.lastName}</span>
+          <span style={Object.assign({}, commonStyle.spanAccountName, this.style.Com_NavSelf_AccountName)}>
+            {this.props.userInfo.firstName + " " + this.props.userInfo.lastName}
+          </span>
         </div>
-        <div>
-          
+        <div
+          style={commonStyle.boxAroundIconLine}>
+          <SvgAroundLineBlank
+            style={{fill: "#e6e6e6", position: 'absolute'}}/>
         </div>
       </div>
     )
