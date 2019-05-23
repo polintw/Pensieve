b1c8183ccc0848c34ef77aa1f5433496f60533ce
polintw <21116945+polintw@users.noreply.github.com>
2019-05-23 19:15:33 +0800

    feat: api for nouns explore, & let NounsBasic action can call callback.

--- app/Within/component/ExploreNouns.jsx
+++ app/Within/component/ExploreNouns.jsx
@@ -4,6 +4,13 @@ import {
   withRouter
 } from 'react-router-dom';
 import {connect} from "react-redux";
+import {
+  handleNounsList
+} from '../../redux/actions/general.js';
+import {
+  cancelErr,
+  uncertainErr
+} from "../../utils/errHandlers.js";
 
 const styleMiddle = {
   comExplore: {
@@ -17,8 +24,12 @@ class ExploreNouns extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      axios: false,
+      listUsed: [],
+      listRandom: []
     };
+    this.axiosSource = axios.CancelToken.source();
+    this._axios_nouns_explore = this._axios_nouns_explore.bind(this);
     this._render_nouns_usedList = this._render_nouns_usedList.bind(this);
     this._render_nouns_randomList = this._render_nouns_randomList.bind(this);
     this.style={
@@ -27,25 +38,75 @@ class ExploreNouns extends React.Component {
   }
 
   _render_nouns_usedList(){
-
+    let list = this.state.listUsed.map((nounId, index)=>{
+      return (
+        <div>
+          {this.props.nounsBasic[nounId].name}
+        </div>
+      )
+    });
+    return list;
   }
 
   _render_nouns_randomList(){
+    let list = this.state.listRandom.map((nounId, index)=>{
+      return (
+        <div>
+          {this.props.nounsBasic[nounId].name}
+        </div>
+      )
+    });
+    return list;
+  }
+
+  _axios_nouns_explore(){
+    const self = this;
+    this.setState({axios: true});
+
+    axios({
+      method: 'get',
+      url: '/router/nouns/explore',
+      headers: {
+        'charset': 'utf-8',
+        'token': window.localStorage['token']
+      },
+      cancelToken: self.axiosSource.cancelToken
+    }).then(function (res) {
+      self.setState({axios: false});
+
+      let resObj = JSON.parse(res.data);
+      let nounsArr = resObj.main.nounsListUsed.concat(resObj.main.nounsListRandom),
+          callBack = ()=>{
+            self.setState({
+              listUsed: resObj.main.nounsListUsed,
+              listRandom: resObj.main.nounsListRandom
+            });
+          };
+
+      self.props._submit_NounsList_new(nounsArr, callBack);
+
+    }).catch(function (thrown) {
+      self.setState({axios: false});
+      if (axios.isCancel(thrown)) {
+        cancelErr(thrown);
+      } else {
+        let message = uncertainErr(thrown);
+        if(message) alert(message);
+      }
+    });
 
   }
 
   componentDidMount() {
     //load nouns from attribution
     //and random nouns as recommandation
-
-    //use api /explore to get nounsList
-    //dispatch to redux get the nounsBasic
-    //promise of dispatch to set nounsList(both from attri. & recommand) into state
-    //then _render
+    this._axios_nouns_explore();
   }
 
   componentWillUnmount() {
-
+    if(this.state.axios){
+      this.axiosSource.cancel("component will unmount.")
+    }
   }
 
   render(){
@@ -66,13 +127,15 @@ class ExploreNouns extends React.Component {
 
 const mapStateToProps = (state)=>{
   return {
-    userInfo: state.userInfo
+    userInfo: state.userInfo,
+    unitCurrent: state.unitCurrent,
+    nounsBasic: state.nounsBasic,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    _submit_UsersList_new: (arr) => { dispatch(handleUsersList(arr)); }
+    _submit_NounsList_new: (arr, callBack) => { dispatch(handleNounsList(arr)).then(()=>{if(callBack) callBack();}); }
   }
 }
 
--- src/nouns/main.js
+++ src/nouns/main.js
@@ -2,10 +2,13 @@ const express = require('express');
 const main = express.Router();
 
 const basicExcutive = require('./basic.js');
+const exploreExecutive = require('./explore.js');
 const searchExecutive = require('./search/execute.js');
 
 main.use('/basic', basicExcutive)
 
 main.use('/search', searchExecutive)
+
+main.use('/explore', exploreExecutive)
 
 module.exports = main;
