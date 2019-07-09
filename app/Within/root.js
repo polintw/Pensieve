dd709c7803eadcd0602fde40137dc51ce104f71a
polintw <21116945+polintw@users.noreply.github.com>
2019-07-09 21:36:15 +0800

    feat: deploy jwtDecode at client(Within) & moment as date().

--- app/Within/root.js
+++ app/Within/root.js
@@ -3,40 +3,59 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from "redux";
 import thunk from 'redux-thunk';
 import {Provider} from "react-redux";
+import jwtDecode from 'jwt-decode';
+import moment from 'moment';
 import Within from './Within.jsx';
 import storeWithin from "../redux/reducers/within.js";
 import {mountUserInfo} from "../redux/actions/general.js";
 
 let loggedin = !!window.localStorage['token'];
 if(loggedin){
-  axios.get('/router/status', {
-    headers: {
-        'token': window.localStorage['token']
-    }
-  }).then(function(res){
-    const store = createStore(storeWithin, applyMiddleware(thunk));
-    store.dispatch(mountUserInfo(res.data.userInfo));
-    ReactDOM.hydrate(<Provider store={store}><Within/></Provider>, document.getElementById("root"));
-  }).catch((err)=>{
-    if (err.response) {
-      // The request was made and the server responded with a status code that falls out of the range of 2xx
-      console.log("response status: "+ err.response.status);
-      if(err.response.status < 500){
-        window.location.assign('/s/signin');
-      }else{
-        alert('Failed: '+err.response.data.message)
+  const statusVefified = ()=>{
+    axios.get('/router/status', {
+      headers: {
+          'token': window.localStorage['token']
       }
-    } else if (err.request) {
-        // The request was made but no response was received
-        // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(err.request);
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', err.message);
-    }
-    console.log("error config: "+err.config);
-  })
+    }).then(function(res){
+      const store = createStore(storeWithin, applyMiddleware(thunk));
+      store.dispatch(mountUserInfo(res.data.userInfo));
+      ReactDOM.hydrate(<Provider store={store}><Within/></Provider>, document.getElementById("root"));
+    }).catch((err)=>{
+      if (err.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log("response status: "+ err.response.status);
+        if(err.response.status < 500){
+          window.location.assign('/s/signin');
+        }else{
+          alert('Failed: '+err.response.data.message)
+        }
+      } else if (err.request) {
+          // The request was made but no response was received
+          // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(err.request);
+      } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', err.message);
+      }
+      console.log("error config: "+err.config);
+    })
+  };
+
+  let decoded = jwtDecode(window.localStorage['token']);
+  let deadline = moment.unix(decoded.exp);
+  let expired = moment().isAfter(deadline);
+
+  if(expired){
+     //send 'refresh' token, which get when last login
+     //return to original process after new token back
+     //and save into localStorage as usual
+   }
+  else{
+    //to the original process
+    statusVefified();
+  }
+
 }else{
   window.location.assign('/s/signin')
 }
