dda1f5f9c93de50011f38435be76c06ec6d7ff87
polintw <21116945+polintw@users.noreply.github.com>
2019-10-01 20:00:24 +0800

    feat: implement NailRegular & Mondriaan in Related, also Origin layout.

--- app/Unit/Related/Related.jsx
+++ app/Unit/Related/Related.jsx
@@ -10,6 +10,7 @@ import classnames from 'classnames';
 import styles from "./styles.module.css";
 import RelatedList from '../RelatedList/RelatedList.jsx';
 import RelatedOrigin from '../RelatedOrigin/RelatedOrigin.jsx';
+import SvgLogo from '../../Component/Svg/SvgLogo.jsx';
 
 const styleMiddle = {
 
@@ -19,11 +20,25 @@ class Related extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      onSpanBack: false
     };
+    this._handleEnter_spanBack = this._handleEnter_spanBack.bind(this);
+    this._handleLeave_spanBack = this._handleLeave_spanBack.bind(this);
     this.style={
 
     }
+  }
+
+  _handleEnter_spanBack(e){
+    this.setState({
+      onSpanBack: true
+    })
+  }
+
+  _handleLeave_spanBack(e){
+    this.setState({
+      onSpanBack: false
+    })
   }
 
   componentDidUpdate(prevProps, prevState, snapshot){
@@ -54,10 +69,38 @@ class Related extends React.Component {
             {...this.props}/>
         </div>
         <div
-          className={classnames(styles.boxList)}>
-          <RelatedList
-            {...this.props}
-            unitId={this.unitId}/>
+          className={classnames(styles.boxUnder)}>
+          <div
+            className={classnames(styles.boxSubtitle, styles.fontSubtitle)}>
+            <span
+              style={{position: 'sticky',top: '27%'}}>
+              {"connect to more"}<br/>{'from'}</span>
+            <div
+              className={classnames(styles.boxLeaveSet)}>
+              <div
+                className={classnames(styles.boxLogo)}
+                onClick={(e)=>{e.preventDefault(); e.stopPropagation(); window.location.assign('/cosmic')}}>
+                <SvgLogo/>
+              </div>
+              <div
+                className={classnames(styles.boxBack)}
+                onMouseEnter={this._handleEnter_spanBack}
+                onMouseLeave={this._handleLeave_spanBack}>
+                <span
+                  className={classnames(styles.spanBack)}
+                  style={this.state.onSpanBack?{color: '#111111'}:{}}
+                  onClick={(e)=>{e.stopPropagation();e.preventDefault();this.props._handleClick_leave();}}>
+                  {" ╳ "}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            className={classnames(styles.boxList)}>
+            <RelatedList
+              {...this.props}
+              unitId={this.unitId}/>
+          </div>
         </div>
         <div
           className={styles.footer}>
