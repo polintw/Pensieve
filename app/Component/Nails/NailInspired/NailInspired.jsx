3b0a2487c430f695120348bce01a8c1f92c9fe54
polintw <21116945+polintw@users.noreply.github.com>
2019-10-05 17:48:40 +0800

    fix: change position prop in NailInspired & mouse e from Enter to Over.

--- app/Component/Nails/NailInspired/NailInspired.jsx
+++ app/Component/Nails/NailInspired/NailInspired.jsx
@@ -17,12 +17,15 @@ class NailInspired extends React.Component {
     super(props);
     this.state = {
       onFrame: false,
-      onImg: false
+      onImg: false,
+      overContent: false
     };
     this.nailImgBox = React.createRef();
     this._render_nails_nouns = this._render_nails_nouns.bind(this);
-    this._handleEnter_nailFrame = this._handleEnter_nailFrame.bind(this);
-    this._handleLeave_nailFrame = this._handleLeave_nailFrame.bind(this);
+    this._handleOver_Content = this._handleOver_Content.bind(this);
+    this._handleOut_Content = this._handleOut_Content.bind(this);
+    this._handleOver_nailFrame = this._handleOver_nailFrame.bind(this);
+    this._handleOut_nailFrame = this._handleOut_nailFrame.bind(this);
     this._handleEnter_nailImg = this._handleEnter_nailImg.bind(this);
     this._handleLeave_nailImg = this._handleLeave_nailImg.bind(this);
     this.style = {
@@ -44,11 +47,23 @@ class NailInspired extends React.Component {
     }
   }
 
-  _handleEnter_nailFrame(e){
+  _handleOver_Content(e){
+    e.stopPropagation(); //stop propagation so the outer frame would not be triggred
+    this.setState({overContent: e.currentTarget.getAttribute('value')});
+  }
+
+  _handleOut_Content(e){
+    e.stopPropagation();
+    this.setState({overContent: false})
+  }
+
+  _handleOver_nailFrame(e){
+    e.stopPropagation();
     this.setState({onFrame: true})
   }
 
-  _handleLeave_nailFrame(e){
+  _handleOut_nailFrame(e){
+    e.stopPropagation();
     this.setState({onFrame: false})
   }
 
@@ -85,19 +100,30 @@ class NailInspired extends React.Component {
         className={classnames(
           styles.frame,
           {[styles.frameOnMouse]: this.state.onFrame}
-        )}
-        onMouseEnter={this._handleEnter_nailFrame}
-        onMouseLeave={this._handleLeave_nailFrame}>
+        )}>
+        <div
+          className={classnames(styles.boxMarkPreview)}>
+          <MarksFive
+            {...this.props}
+            overContent={this.state.overContent}
+            _handleOver_Content={this._handleOver_Content}
+            _handleOut_Content={this._handleOut_Content}/>
+        </div>
         <Link
           to={{
             pathname: this.props.linkPath,
             search: '?theater&unitId='+this.props.unitId,
             state: {from: this.props.location}
           }}
-          className={classnames('plainLinkButton', styles.boxUnit)}>
+          className={classnames('plainLinkButton')}
+          onMouseOver={this._handleOver_nailFrame}
+          onMouseOut={this._handleOut_nailFrame}>
           <div
             ref={this.nailImgBox}
-            className={styles.boxImg}
+            className={classnames(
+              styles.boxImg,
+              {[styles.boxImgNailWide]: (this.props.unitBasic.marksList.length > 1)}
+            )}
             onMouseEnter={this._handleEnter_nailImg}
             onMouseLeave={this._handleLeave_nailImg}>
             <ImgPreview
@@ -117,62 +143,71 @@ class NailInspired extends React.Component {
             </div>
           </div>
           <div
-            className={styles.boxNodes}>
+            className={classnames(
+              styles.boxNodes,
+              {[styles.boxNodesNailWide]: (this.props.unitBasic.marksList.length > 1)}
+            )}>
             {this._render_nails_nouns()}
           </div>
         </Link>
-        <div
-          className={classnames(styles.boxMarkPreview)}>
-          {_render_Marks_Less5(this.props)}
-        </div>
-
       </div>
     )
   }
 }
 
-function _render_Marks_Less5(props){
-  //the mark(s), would be put in a rectangle 5vw left to right border,
-  //but start from the bottom corner(beneath the img)
-  let marksDOM = [];
-  const plainDOM = (num)=>{ //the plain block used to fill in the empty place
-    return(
-      <div
-        key={"key_nailInspired_plain_"+num+"_ofUnit_"+props.unitId}
-        className={styles.boxMark}/>
-    )
-  };
+class MarksFive extends React.Component {
+  constructor(props){
+    super(props);
+  }
 
-  //First, render the mark, only the first 5
-  //remember to add a 'hint' in the fututre if there are more than 5
-  for(let i=0 ; i< props.unitBasic.marksList.length && i< 5; i++){
-    let markKey = props.unitBasic.marksList[i]
-    marksDOM.push(
-      <div
-        key={"key_nailInspired_mark_"+i+"_ofUnit_"+props.unitId}
-        className={classnames(styles.boxMark, 'fontNailMark', styles.fontMark)}>
-        <Link
-          to={{
-            pathname: props.match.url+"/unit",
-            search: '?theater&unitId='+props.unitId+"&mark="+markKey,
-            state: {from: props.location}
-          }}
-          className={classnames('plainLinkButton', styles.boxContent)}>
-          <DisplayMarkPreview
-            rawContent={props.marksBasic[markKey].editorContent}/>
-        </Link>
-      </div>
-    );
-  };
+  render(){
+    //the mark(s), would be put in a rectangle 5vw left to right border,
+    //but start from the bottom corner(beneath the img)
+    let marksDOM = [];
+    const plainDOM = (num)=>{ //the plain block used to fill in the empty place
+      return(
+        <div
+          key={"key_nailInspired_plain_"+num+"_ofUnit_"+this.props.unitId}
+          className={styles.boxMark}/>
+      )
+    };
 
-  //than, important! add the filling,
-  //1 or 2 depend on the number of the marks,
-  //2 filings would render the empty place next to the Nodes.
-  //It is desired if there are even number of marks.
-  if(!!(props.unitBasic.marksList.length % 2)) marksDOM.unshift(plainDOM(0))
-  else {marksDOM.unshift(plainDOM(1)); marksDOM.unshift(plainDOM(0)); };
+    //First, render the mark, only the first 5
+    //remember to add a 'hint' in the fututre if there are more than 5
+    for(let i=0 ; i< this.props.unitBasic.marksList.length && i< 5; i++){
+      let markKey = this.props.unitBasic.marksList[i]
+      marksDOM.push(
+        <div
+          key={"key_nailInspired_mark_"+i+"_ofUnit_"+this.props.unitId}
+          className={classnames(styles.boxMark, 'fontNailMark', styles.fontMark)}>
+          <Link
+            to={{
+              pathname: this.props.match.url+"/unit",
+              search: '?theater&unitId='+this.props.unitId+"&mark="+markKey,
+              state: {from: this.props.location}
+            }}
+            value={markKey}
+            className={classnames('plainLinkButton', styles.boxContent)}
+            style={(this.props.overContent==markKey) ? {outline: 'double 1.5px #777777'}: {}}
+            onMouseOver={this.props._handleOver_Content}
+            onMouseOut={this.props._handleOut_Content}>
+            <DisplayMarkPreview
+              rawContent={this.props.marksBasic[markKey].editorContent}/>
+          </Link>
+        </div>
+      );
+    };
 
-  return marksDOM;
+    //than, important! add the filling,
+    //1 or 2 depend on the number of the marks,
+    //2 filings would render the empty place next to the Nodes.
+    //It is desired if there are even number of marks.
+    if(!!(this.props.unitBasic.marksList.length % 2)) marksDOM.unshift(plainDOM(0))
+    else {marksDOM.unshift(plainDOM(1)); marksDOM.unshift(plainDOM(0)); };
+
+    return marksDOM;
+
+  }
 }
 
 
