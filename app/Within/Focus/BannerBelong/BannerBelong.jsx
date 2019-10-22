7629a8e4cf563b1cb5595fcc796362308453d70a
polintw <21116945+polintw@users.noreply.github.com>
2019-10-22 22:32:27 +0800

    feat: new api records/nodes first used by BannerBelong.

--- app/Within/Focus/BannerBelong/BannerBelong.jsx
+++ app/Within/Focus/BannerBelong/BannerBelong.jsx
@@ -17,28 +17,31 @@ import {
 const recordLink = (nodeId, self)=>{
   //still check if the node has data in reducer
   return (
-    <Link
-      key={"key_Belong_records_"+nodeId}
-      to={"/cosmic/nodes/"+nodeId}
-      nodeid={nodeId}
-      className={classnames('plainLinkButton', styles.boxRecord)}
-      onMouseEnter={self._handleEnter_Record}
-      onMouseLeave={self._handleLeave_Record}>
-      <div
-        className={classnames(styles.spanRecord)}>
-        {
-          (self.state.onRecord== nodeId) &&
-          <span style={{
-              width: '72%', position: 'absolute', bottom: '-11%', left: '5%',
-              borderBottom: 'solid 1px #ff7a5f'
-            }}/>
-          }
-        {nodeId in self.props.nounsBasic ? (
-          self.props.nounsBasic[nodeId].name) : (
-            null
-          )}
-      </div>
-    </Link>
+    <div>
+      <
+      <Link
+        key={"key_Belong_records_"+nodeId}
+        to={"/cosmic/nodes/"+nodeId}
+        nodeid={nodeId}
+        className={classnames('plainLinkButton', styles.boxRecord)}
+        onMouseEnter={self._handleEnter_Record}
+        onMouseLeave={self._handleLeave_Record}>
+        <div
+          className={classnames(styles.spanRecord)}>
+          {
+            (self.state.onRecord== nodeId) &&
+            <span style={{
+                width: '72%', position: 'absolute', bottom: '-11%', left: '5%',
+                borderBottom: 'solid 1px #ff7a5f'
+              }}/>
+            }
+            {nodeId in self.props.nounsBasic ? (
+              self.props.nounsBasic[nodeId].name) : (
+                null
+              )}
+            </div>
+          </Link>
+    </div>
   )
 }
 
@@ -48,9 +51,14 @@ class BannerBelong extends React.Component {
     this.state = {
       axios: false,
       onRecord: false,
+      nodesChart: {},
+      nodesList: []
     };
+    this._set_nodesShared = this._set_nodesShared.bind(this);
     this._render_BelongList = this._render_BelongList.bind(this);
+    this._axios_GET_sharedCount = this._axios_GET_sharedCount.bind(this);
     this._axios_GET_belongRecords = this._axios_GET_belongRecords.bind(this);
+    this._axios_GET_recordeShared = this._axios_GET_recordeShared.bind(this);
     this._handleEnter_Record = this._handleEnter_Record.bind(this);
     this._handleLeave_Record = this._handleLeave_Record.bind(this);
   }
@@ -68,33 +76,77 @@ class BannerBelong extends React.Component {
   }
 
   _axios_GET_belongRecords(){
-    const self = this;
-    this.setState({axios: true});
-
-    axios({
+    return axios({
       method: 'get',
-      url: '/router/profile/sheetsNodes?present&random&limit=5',
+      url: '/router/profile/sheetsNodes?present&random&limit=8',
       headers: {
         'charset': 'utf-8',
         'token': window.localStorage['token']
       },
-      cancelToken: self.axiosSource.cancelToken
-    }).then(function (res) {
-      let resObj = JSON.parse(res.data);
-      self.props._submit_NounsList_new(resObj.main.nodesList);
+      cancelToken: this.axiosSource.cancelToken
+    })
+  }
 
-      self.setState((prevState, props)=>{
-        return({
-          axios: false,
-          records: resObj.main.nodesList.length> 0 ? resObj.main.nodesList : true,
-          //check if the nodesList has anything. if not, return true to let the com rendered(default 'false')
-          viewForm: resObj.main.nodesList.length> 0 ? false : true
-          //if nothing, means need to show the ptions Form, set it to 'true'
+  _axios_GET_recordeShared(){
+    return axios({
+      method: 'get',
+      url: '/router/records/nodes?type=shared&limit=5', //the limit 5, considering the possibility of 'repeat' to belong list
+      headers: {
+        'charset': 'utf-8',
+        'token': window.localStorage['token']
+      },
+      cancelToken: this.axiosSource.cancelToken
+    })
+  }
+
+  _axios_GET_sharedCount(nodeId){
+    return axios({
+      method: 'get',
+      url: '/router/nouns/'+nodeId+ '/attribution',
+      params: {require: 'countShared'},
+      headers: {
+        'charset': 'utf-8',
+        'token': window.localStorage['token']
+      },
+      cancelToken: this.axiosSource.cancelToken
+    })
+  }
+
+  _set_nodesShared(nodesList){
+
+  }
+
+  componentDidMount() {
+    const self = this;
+    this.setState({axios: true});
+
+    axios.all([
+      this._axios_GET_belongRecords(),
+      this._axios_GET_recordeShared()
+    ]).then(
+      axios.spread((belongRecord, recordShared)=>{
+        let belongObj = JSON.parse(belongRecord.data),
+            sharedObj = JSON.parse(recordShared.data);
+        let sharedList = sharedObj.main.nodesList;
+        if(sharedList.length > belongObj.main.nodesList.length){ //long enouogh to delete repeat node by belong
+          sharedList = sharedList.filter((nodeId, index)=>{ return belongObj.main.nodesList.indexOf(nodeId)< 0 })
+        }
+        //then, concat the lists
+        const nodesList= belongObj.main.nodesList.concat(sharedList);
+
+        self.props._submit_NounsList_new(nodesList); //GET nodes info by Redux action
+        self._set_nodesShared(nodesList); //GET count of each node display
+
+        self.setState((prevState, props)=>{
+          return({
+            axios: false,
+            nodesList: nodesList,
+            nodesChart: resObj.main.nodesChart
+          });
         });
-        //for now, we didn't update nodes connection to user to anywhere
-        //even it was already prepared by res data
-      });
-    }).catch(function (thrown) {
+
+      })
+    ).catch(function (thrown) {
       self.setState({axios: false});
       if (axios.isCancel(thrown)) {
         cancelErr(thrown);
@@ -105,10 +157,6 @@ class BannerBelong extends React.Component {
     });
   }
 
-  componentDidMount() {
-    this._axios_GET_belongRecords();
-  }
-
   componentWillUnmount() {
     if(this.state.axios){
       this.axiosSource.cancel("component will unmount.")
@@ -116,7 +164,11 @@ class BannerBelong extends React.Component {
   }
 
   _render_BelongList(){
-    let items = this.state.records.map((nodeId, index)=>{
+    ["residence", "stay", "hometown", "", "used", "used"]
+
+    let items = this.state.nodesList.map((nodeId, index)=>{
+
+      this.state.nodesChart[nodeId]
       return recordLink(nodeId, this);
     });
 
@@ -125,14 +177,9 @@ class BannerBelong extends React.Component {
 
   render(){
     return(
-      <div>
-        {
-          !this.state.viewForm && this.state.records &&
-          <div
-            className={classnames(styles.boxList, styles.fontList)}>
-            {this._render_BelongList()}
-          </div>
-        }
+      <div
+        className={classnames(styles.boxList, styles.fontList)}>
+        {this._render_BelongList()}
       </div>
     )
   }
@@ -140,13 +187,16 @@ class BannerBelong extends React.Component {
 
 const mapStateToProps = (state)=>{
   return {
-    userInfo: state.userInfo
+    userInfo: state.userInfo,
+    unitCurrent: state.unitCurrent,
+    i18nUIString: state.i18nUIString,
+    nounsBasic: state.nounsBasic,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    _submit_NounsList_new: (arr) => { dispatch(handleNounsList(arr)); },
   }
 }
 
