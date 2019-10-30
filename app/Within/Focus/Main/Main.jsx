b9886687c3cb1573f4d4dd9d84b3b488c8386a92
polintw <21116945+polintw@users.noreply.github.com>
2019-10-30 21:39:30 +0800

    hotfix, correct route path used by Main to Unit.

--- app/Within/Focus/Main/Main.jsx
+++ app/Within/Focus/Main/Main.jsx
@@ -270,7 +270,7 @@ class MainIndex extends React.Component {
           </div>
         </div>
         <Route
-          path={this.props.match.path+"/unit"}
+          path={"/unit"}
           render={(props)=> <Unit {...props} _construct_UnitInit={this._construct_UnitInit} _refer_von_unit={this.props._refer_von_cosmic}/>}/>
       </div>
     )
