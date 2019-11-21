42ebf090d8c1f3830a75e0556fedbb8ac16a2c0b
polintw <21116945+polintw@users.noreply.github.com>
2019-11-21 22:21:24 +0800

    refact: clean all remaining related to 'broad' of previous version.

--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ const auth = require('./auth/main.js');
 const accountRouter = require('./account/main.js');
 const userRouter = require('./user/main.js');
 const actionsRouter = require('./user/actions/main.js');
-const embeddedRouter = require('./user/embedded/main.js');
 const collateralsRouter = require('./user/collaterals/main.js');
 const scapeRouter = require('./user/scape/main.js');
 const unitRouter = require('./unit/main.js');// shift to "units"
@@ -33,7 +32,6 @@ router.use('/', auth)
 //only approach after verified
 router.use('/user', userRouter)
 router.use('/actions', actionsRouter) //prepare to replace part of the "/user"
-router.use('/embedded', embeddedRouter) //prepare to replace part of the "/user"
 router.use('/collaterals', collateralsRouter) //prepare to replace part of the "/user"
 router.use('/scape', scapeRouter) //prepare to replace part of the "/user"
 
