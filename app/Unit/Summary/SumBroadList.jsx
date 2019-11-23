9bef01800d5693a45216a81827df609903c7e41e
polintw <21116945+polintw@users.noreply.github.com>
2019-11-23 17:43:38 +0800

    refactor: est. new file SumBroadList in /Summary.

--- app/Unit/Summary/SumBroadList.jsx
+++ app/Unit/Summary/SumBroadList.jsx
@@ -6,13 +6,12 @@ import {
 import {connect} from "react-redux";
 import classnames from 'classnames';
 import styles from './styles.module.css';
-import {setUnitBroad} from '../../redux/actions/general.js';
 import {
   cancelErr,
   uncertainErr
 } from "../../utils/errHandlers.js";
 
-class ActionBroad extends React.Component {
+class SumBroadList extends React.Component {
   constructor(props){
     super(props);
     this.state = {
@@ -22,9 +21,7 @@ class ActionBroad extends React.Component {
     this.axiosSource = axios.CancelToken.source();
     this._handleEnter_act = this._handleEnter_act.bind(this);
     this._handleLeave_act = this._handleLeave_act.bind(this);
-    this._handleClick_Broad = this._handleClick_Broad.bind(this);
     this._axios_post_actionBroad = this._axios_post_actionBroad.bind(this);
-    this._axios_patch_actionBroad = this._axios_patch_actionBroad.bind(this);
     this.style={
 
     };
@@ -67,39 +64,6 @@ class ActionBroad extends React.Component {
     });
   }
 
-  _axios_patch_actionBroad(){
-    const self = this;
-    this.setState({axios: true});
-
-    axios.patch('/router/broad/'+this.props.unitCurrent.unitId, {}, {
-      headers: {
-        'charset': 'utf-8',
-        'token': window.localStorage['token']
-      },
-      cancelToken: self.axiosSource.token
-    }).then((res)=>{
-      self.setState({axios: false});
-      //submit change to reducer
-      self.props._submit_Broad_status({broad: false});
-
-    }).catch(function (thrown) {
-      self.setState({axios: false});
-      if (axios.isCancel(thrown)) {
-        cancelErr(thrown);
-      } else {
-        let message = uncertainErr(thrown);
-        if(message) alert(message);
-      }
-    });
-  }
-
-  _handleClick_Broad(event){
-    event.preventDefault();
-    event.stopPropagation();
-    if(this.props.unitCurrent.broad) this._axios_patch_actionBroad()
-    else this._axios_post_actionBroad();
-  }
-
   componentDidMount(){
 
   }
@@ -113,23 +77,8 @@ class ActionBroad extends React.Component {
   render(){
     return(
       <div
-        className={classnames(styles.comActionBroad)}
-        style={Object.assign(
-          {},
-          {cursor: 'pointer'},
-          (this.props.unitCurrent.broad || this.state.onAct)? {color: '#FAFAFA'}:{color: 'rgba(250,250,250,0.5)'}
-        )}
-        onClick={this._handleClick_Broad}
-        onMouseEnter={this._handleEnter_act}
-        onMouseLeave={this._handleLeave_act}>
-        <span
-          className={classnames()}>
-          {"Broad"}
-        </span>
-        <span
-          className={classnames()}>
-          {"cast"}
-        </span>
+        className={classnames(styles.comSum_BroadList)}>
+
       </div>
     )
   }
@@ -144,11 +93,11 @@ const mapStateToProps = (state)=>{
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    _submit_Broad_status: (obj) => { dispatch(setUnitBroad(obj)); }
+
   }
 }
 
 export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(ActionBroad));
+)(SumBroadList));
