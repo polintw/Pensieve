6352e461c831a28bf86b8e9affe0ed6340bfc680
polintw <21116945+polintw@users.noreply.github.com>
2019-12-16 18:30:36 +0800

    feat: est. and finish api GET matchNodes/list /user & /taking.

--- src/matchNodes/list.js
+++ src/matchNodes/list.js
@@ -4,32 +4,96 @@ const winston = require('../../config/winston.js');
 
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const _DB_broads = require('../../db/models/index').broads;
-const _DB_usersCustomIndex = require('../../db/models/index').users_custom_index;
-const {_res_success} = require('../utils/resHandler.js');
+const _DB_usersDemandMatch = require('../../db/models/index').users_demand_match;
+const _DB_nodesDemandMatch = require('../../db/models/index').nodes_demand_match;
+const {_res_success} = require('../../utils/resHandler.js');
 const {
   _handle_ErrCatched,
   internalError,
 } = require('../utils/reserrHandler.js');
 
-function _handle_GET_feed_mainBroads(req, res){
+function _handle_GET_list_userNodes(req, res){
   new Promise((resolve, reject)=>{
     const userId = req.extra.tokenUserId;
 
-    _DB_usersCustomIndex.findOne({
-      where: {
-        id_user: userId
-      },
-      attributes: ['last_visit', 'id_user']
+    let desiredList;
+    switch (req.query.desire) {
+      case 'willing':
+        desiredList = 'list_willing'
+        break;
+      case 'wished':
+        desiredList = 'list_wished'
+        break;
+      default:
+        desiredList =''
+    };
 
-    }).then((sendingData)=>{
+    _DB_usersDemandMatch.findOne({
+      where: {id_user: userId}
+    }).then((selectResult)=>{
+      let sendingData ={
+        nodesList: [],
+        temp:{}
+      };
+      sendingData.nodesList = JSON.parse( selectResult[desiredList] );
 
       resolve(sendingData);
     }).catch((err)=>{
       reject(new internalError(err, 131));
     });
+
   }).then((sendingData)=>{
-    _res_success(res, sendingData, "Complete, GET: /feed/broads.");
+    _res_success(res, sendingData, "Complete, GET: /list/user.");
+  }).catch((error)=>{
+    _handle_ErrCatched(error, req, res);
+  });
+}
+
+
+function _handle_GET_list_taking(req, res){
+  new Promise((resolve, reject)=>{
+    const userId = req.extra.tokenUserId;
+
+    _DB_usersDemandMatch.findOne({
+      where: {id_user: userId}
+    })
+    .then((selectResult)=>{
+      let nodeTaken = selectResult.taking[0];
+      let sendingData ={
+        nodesList: [],
+        demandCount: null,
+        temp:{}
+      };
+
+      if(!nodeTaken){
+        return sendingData; //if no taking on the record, return to next step directly
+      }
+      else{
+        return _DB_nodesDemandMatch.findOne({
+          where: {id_node: nodeTaken}
+        })
+        .then((nodeRow)=>{
+          let demandList = JSON.parse(nodeRow.list_demand),
+              waitingList = JSON.parse(nodeRow.list_waiting);
+
+          sendingData.nodesList.push(nodeRow.id_node);
+          sendingData.demandCount = (demandList.length + waitingList.length);
+
+          return sendingData;
+        })
+        .catch((err)=>{throw err});
+      }
+
+    })
+    .then((sendingData)=>{
+      //resolve if no rejection
+      resolve(sendingData);
+    }).catch((err)=>{
+      reject(new internalError(err, 131));
+    });
+
+  }).then((sendingData)=>{
+    _res_success(res, sendingData, "Complete, GET: /list/user.");
   }).catch((error)=>{
     _handle_ErrCatched(error, req, res);
   });
@@ -38,8 +102,12 @@ function _handle_GET_feed_mainBroads(req, res){
 
 execute.get('/', function(req, res){
   if(process.env.NODE_ENV == 'development') winston.verbose('matchNodes, GET: /list/user');
+  _handle_GET_list_userNodes(req, res);
+})
 
-
+execute.get('/taking', function(req, res){
+  if(process.env.NODE_ENV == 'development') winston.verbose('matchNodes, GET: /list/taking');
+  _handle_GET_list_taking(req, res);
 })
 
 module.exports = execute;
