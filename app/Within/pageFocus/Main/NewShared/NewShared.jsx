cadc7860198f188387eb22f1b43700595f59cf33
polintw <21116945+polintw@users.noreply.github.com>
2019-12-23 20:07:56 +0800

    feat: set NewShared base on NewSharedCustom.

--- app/Within/pageFocus/Main/NewShared/NewShared.jsx
+++ app/Within/pageFocus/Main/NewShared/NewShared.jsx
@@ -16,6 +16,9 @@ import {
   handleUsersList
 } from "../../../../redux/actions/general.js";
 import {
+  setFlag
+} from '../../../../redux/actions/cosmic.js';
+import {
   cancelErr,
   uncertainErr
 } from '../../../../utils/errHandlers.js';
@@ -29,37 +32,114 @@ class NewShared extends React.Component {
       marksBasic: {}
     };
     this.axiosSource = axios.CancelToken.source();
-
+    this._fetch_Units = this._fetch_Units.bind(this);
+    this._render_unitsNew = this._render_unitsNew.bind(this);
     this.style={
 
     }
   }
 
+  _fetch_Units(unitsList){
+    const self = this;
+    this.setState({axios: true});
+
+    axios_Units(this.axiosSource.token, unitsList)
+    .then((resObj)=>{
+      //after res of axios_Units: call get nouns & users
+      self.props._submit_NounsList_new(resObj.main.nounsListMix);
+      self.props._submit_UsersList_new(resObj.main.usersList);
+      //and update the data of units to state
+      self.setState((prevState, props)=>{
+        return ({
+          axios: false,
+          unitsBasic: {...prevState.unitsBasic, ...resObj.main.unitsBasic},
+          marksBasic: {...prevState.marksBasic, ...resObj.main.marksBasic}
+        });
+      });
+
+    })
+    .catch(function (thrown) {
+      self.setState({axios: false});
+      if (axios.isCancel(thrown)) {
+        cancelErr(thrown);
+      } else {
+        let message = uncertainErr(thrown);
+        if(message) alert(message);
+      }
+    });
+
+  }
+
 
   componentDidUpdate(prevProps, prevState, snapshot){
-
+    if(this.props.flagNewSharedDataFetch && this.props.flagNewSharedDataFetch != prevProps.flagNewSharedDataFetch){
+      this._fetch_Units(this.props.indexLists.listNew);
+      this.props._submit_FlagSwitch(['flagNewSharedDataFetch']); //set flag back to dafault
+    }
   }
 
   componentDidMount() {
-
+    if(this.props.flagNewSharedDataFetch){
+      this._fetch_Units(this.props.indexLists.listNew);
+      this.props._submit_FlagSwitch(['flagNewSharedDataFetch']); //set flag back to dafault
+    }
   }
 
   componentWillUnmount() {
-
+    if(this.state.axios){
+      this.axiosSource.cancel("component will unmount.")
+    }
   }
 
+  _render_unitsNew(){
+    //our list was saved to reducer after fetch
+    let unitsList = this.props.indexLists['listNew'],
+        unitsDOM = [];
+    if(unitsList.length > 0 ){ // check necessity first, skip if no item.
+      //we render no more than 6, but the backend may pass more than 6, so don't forget setting the limit
+      for(let i =0 ; i< 6 && i< unitsList.length; i++){ //again, don't forget the length limit to prevent error cause by unwanted cycle
+        let unitId = unitsList[i];
+        //then important question: do we have the data of this Unit ? if not, we skip to next one
+        if(unitId in this.state.unitsBasic) {
+          let nail = nailChart(2, unitId, this);
+          unitsDOM.push(nail);
+        }
+      }
+    }
+
+    return unitsDOM;
+  }
 
   render(){
-
+    return(
+      <div>
+        <div>{"title"}</div>
+        <div>
+          {this._render_unitsNew()}
+        </div>
+      </div>
+    )
   }
 }
 
 const mapStateToProps = (state)=>{
-
+  return {
+    userInfo: state.userInfo,
+    indexLists: state.indexLists,
+    unitCurrent: state.unitCurrent,
+    i18nUIString: state.i18nUIString,
+    flagNewSharedRefresh: state.flagNewSharedRefresh,
+    nounsBasic: state.nounsBasic,
+    usersBasic: state.usersBasic,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-
+  return {
+    _submit_NounsList_new: (arr) => { dispatch(handleNounsList(arr)); },
+    _submit_UsersList_new: (arr) => { dispatch(handleUsersList(arr)); },
+    _submit_FlagSwitch: (target) => { dispatch(setFlag(target)); },
+  }
 }
 
 export default withRouter(connect(
