bc74b7d7b36fd8e2803717321747b7494485a0f0
ycc02 <octopus0211@gmail.com>
2020-06-09 16:28:42 +0800

    feat: eng. word change

--- app/redux/states/i18nUIString.js
+++ app/redux/states/i18nUIString.js
@@ -3,69 +3,69 @@ const i18nUIString_EN = {
   "descript_Unit_Author_broad": ["broad by", "broad"],
 
   "guidingSign_Confirm_Success": ["Email address has been successfully verified.", "Sign in now and start your adventure, to infinity and beyond!"],
-  "guidingSign_Confirm_Fail": ["Email address verified failed.", "It's probablly due to the valid time was over.", "Or, haven't received the verified mail?"],
-  "link_Sign_mailResend": "sending verified mail again",
-  "title_Sign_mailResend": ["Re-send Verified Mail", "Forget Password"],
-  "title_Sign_pwReset": "Reset New Password.",
-  "message_Resend_PassReset": "Your password has been updated. Please Sign in with new password. ",
+  "guidingSign_Confirm_Fail": ["Email address verification failed.", "You are probablly over the valid time.", "Or, haven't received the verification email?"], //Original:"It's probablly due to the valid time was over.", "Or, haven't received the verified mail?"
+  "link_Sign_mailResend": "Re-send verification email.", //Original: "sending verified mail again"
+  "title_Sign_mailResend": ["Re-send Verification Mail", "Forget Password"], //Original: "Verified"
+  "title_Sign_pwReset": "Reset Password.", //Original: "Reset New Password."
+  "message_Resend_PassReset": "Your password has been updated. Please Sign in with the new password. ", //Add "the" before new
 
-  "title_Signup_Success": "Success! Please Verifying Your Mail Address.",
-  "title_Signup_VefiryFail": "Verified fail.",
+  "title_Signup_Success": "Success! Please Verify Your Email Address.", //Original: "Success! Please Verifying Your Mail Address."
+  "title_Signup_VefiryFail": "Verification fail.", //Original: "Verified fail."
   "title_Signup_VefiryConfirm": "Complete!",
   "guidingSign_Signup_Success": [
-    "Your registration was submit successfully.",
-    "Please verifing your mail address by link we just sent to you. That's all you need to do before log in.",
-  ],
+    "Your registration was submitted successfully.", //"Your registration was submit successfully."
+    "Please verify your email address with the link sent to your mailbox.", 
+  ],//"Please verifing your mail address by link we just sent to you. That's all you need to do before log in." 後面這句感覺不太需要?!
   "message_Signin_intro": ["Get Start!", "before you miss the wonder of the world."],
 
-  "subtitle_Sign_name": ["First name", "Family name"],
+  "subtitle_Sign_name": ["First name", "Last name"], //Original: Family name. 但Last name比較常用
   "subtitle_Sign_gender": "Gender",
-  "subtitle_Sign_emailResend": "Email of your account",
-  "hint_Signup_gendeSelect": "How do you want us to call you?",
+  "subtitle_Sign_emailResend": "Email of your account", //????????????
+  "hint_Signup_gendeSelect": "How do you want us to call you?", //??? Nickname
   "options_genderPronoun": ["He/Him", "She/Her"],
   "message_Signup_Form": [
-    "Please provide an valid email address",
+    "Please provide a valid email address", //Change "an" to "a"
     "First and Last name are required.",
-    "please choose your gender, or select desired pronoun by 'Others'",
-    'Password must be more than 8 chars and incl. at least 1 letter and 1 number',
-    "Not the same as above"
+    "please choose your gender, or select desired pronoun by 'Others'", //???????????
+    'Your password must be between 8 and 30 characters. Your password must contain at least one letter and one number digit.', //Original: 'Password must be more than 8 chars and incl. at least 1 letter and 1 number'
+    "The password and confirmation password do not match." //Original:"Not the same as above"
   ],
   "descript_Sign_termsDeclaim": [
     "By clicking “Register”, you agree our", " Terms of Services", " and ", "Privacy Policy", "."],
   "submit_nav_Signin": "Sign in",
   "submit_nav_Signup": "Sign up",
 
-  "title_onBoard_Welcome": "welcome, ",
+  "title_onBoard_Welcome": "Welcome, ",
   "descript_onBoard_Intro": ["Cornerth. brings you info from your homeland and current stay.", "Share what’s happening around you and keep up your steps to the fellows from the other side!"],
-  "descript_onBoard_BelongsHint": ["Before you start …", "Tell us where are you from?"],
-  "guideing_onBoard_BelongsHint": ["I'm from", "Now based at"],
-  "submit_onBoard_start": "Let's Start",
-  "submit_Invite_": "Fellow Inviting!",
-  "descript_Invite_": ["Any people you knew born or lived around you? ", "Inviting these fellows to join you!"],
-  "guiding_Invite_": ["Choosing the characteristic of people you want to invite", "You havn't set the place you belong to yet."],
+  "descript_onBoard_BelongsHint": ["Before start …", "Tell us, where are you from?"], //Original: "Before you start …", "Tell us where are you from?"
+  "guideing_onBoard_BelongsHint": ["I'm from", "Living in"], //Original:"Now based at"
+  "submit_onBoard_start": "Let's Get Started", //Original:"Let's Start"
+  "submit_Invite_": "Fellow Invitation!", //Original:"Fellow Inviting!"
+  "descript_Invite_": ["Any one you know stay or was born in the same country or city? ", "Invite these fellows to join us!"], //Original:"Any people you knew born or lived around you? ", "Inviting these fellows to join you!"
+  "guiding_Invite_": ["Choosing the characteristic of people you want to invite", "You havn't set the place you belong to yet."],//????????
   "message_Invite_fellows": [
-    "Hi, there. Here is ",
-    ". And I would like to invite you to experience the Cornerth. with me as you're ",
-    "coming from ", "staying at ",
-    "as well! Joining me by Signing up and see you in there!",
+    "Hi, there. This is ", //Original: "Here is"
+    ". I would like to invite you to experience the Cornerth. with me as you're ", //Deleted "And" in the beginning
+    "from ", "staying at ", //Original: "coming from"
+    "as well! Join me by Signing up today on Cornerth. See you there!", //Original:"as well! Joining me by Signing up and see you in there!"
   ],
   "message_Invite_General": "It's a place for you connecting the environment you are. Signing up and join your fellows in this confused world!",
-
+//??????????
   "category_Belongs_": ["Homeland", "Current Stay"],
 
   "button_complete": "Complete",
 
   "message_hello": "Hi, ",
   "submit_": "Submit",
-  "submit_back": "back",
-  "submit_complete": "complete",
-  "submit_close": "close",
-  "submit_cancel": "cancel",
-  "submit_change": "change",
+  "submit_back": "Back", //Original:"back"
+  "submit_complete": "Complete", //Original: "complete"
+  "submit_close": "Close", //Original: "close"
+  "submit_cancel": "Cancel", //Original: "cancel"
+  "submit_change": "Change", //Original: "change"
   "submit_edit": "Edit",
   "submit_save": "Save",
-  "submit_erase": "erase",
-  "submit_understand": "Understand",
+  "submit_erase": "erase", //感覺用Delete比較好
+  "submit_understand": "Understand",//感覺用Close比較好
   "submit_respond": "Respond",
   "submit_logout": "Log out",
   "title_share": "Share",
@@ -73,40 +73,40 @@ const i18nUIString_EN = {
   "title_home": "Home",
   "subtitle_email": "Email",
   "subtitle_Password": "Password",
-  "link_UnitListResponds": 'responds',
+  "link_UnitListResponds": 'Responds',//Original:'responds'
   "Cornerth_inc": "Cornerth., Inc. ",
   "AllRights": "All Rights Reserved.",
 
   "submit_Options_profile": "My profile",
   "submit_Unit_upload": ["Add image", "Processing..."],
   "descript_Unit_Primer": ["Based on ", "'s Unit", "Based on a published Unit"],
-  "guiding_UnitEdit_upload": "Get started by uploading image about your topic!",
-  "guiding_UnitEdit_imgNoMark": "keep it clean or by Clicking make some Post-it without hesitate!",
-  "guiding_placeholder_UnitEdit_MarkBlock": "What's SPECIAL!  Telling us some details you found." ,
-  "message_UnitEdit_tooManySpot": "A better spot? But too many paragraph would be a burdon to readers. Please delete an old one before you implement this new!",
+  "guiding_UnitEdit_upload": "Get started by uploading an image!", //Original:"Get started by uploading image about your topic!"
+  "guiding_UnitEdit_imgNoMark": "keep the photo clean or by Clicking on the picture to markup your thought without hesitation!", //Original:"keep it clean or by Clicking make some Post-it without hesitate!"
+  "guiding_placeholder_UnitEdit_MarkBlock": "What's SPECIAL!  Tell us about details you found." , //Original:Telling us some details you found."
+  "message_UnitEdit_tooManySpot": "A better spot? But too many paragraph would be a burdon to readers. Please delete an old one before you implement this new!",//?????
   "message_UnitEdit_ruleAssignedNodes": ["Select only one corner from where you stay, and one from homeland."],
 
   "title_shareUpload_typewriter": ['S','u','b','m','i', 't'],
-  "guidingCreateShare_AssignGroup": "corner(s) you'd like to contribute",
-  "guidingCreateShare_AssignNull": "please set corners you belong first.",
-  "message_Unit_Editing_AssignNotAllowed": "the corner linked to could Not be changed after first release.",
-  "message_Unit_EraseConfirm": ["No one would see this Shared after erase, including you. ", "Are you sure your going to erase this Shared?"],
+  "guidingCreateShare_AssignGroup": "Corner(s) you'd like to contribute",//Changed "corner(s)" to "Corner(s)"
+  "guidingCreateShare_AssignNull": "Please set corners you belong first.",//Changed "please" to "Please"
+  "message_Unit_Editing_AssignNotAllowed": "The corner linked to could Not be changed after first release.",//Changed "the" to "The"?????
+  "message_Unit_EraseConfirm": ["No one can see this Shared after erase, including you. ", "Are you sure you want to erase this Shared?"], //Original:"No one would see this Shared after erase, including you. ", "Are you sure your going to erase this Shared?"
   "message_Unit_EraseRes": ["This Shared has beed erased successfully."],
   "title_Unit_ListResponds": "Responds",
-  "guiding_Unit_Responds_authorEmpty": "Respond to your contribution would show up here!",
+  "guiding_Unit_Responds_authorEmpty": "Respond to your contribution will show up here!",//Changed "would" to "will"
   "guiding_Unit_Responds_author": "More responds would be listed!",
   "guiding_Unit_Responds_none": "Submit your respond to this article!",
-  "title_FeedAssigned_AllRead": "you've all browsed.",
+  "title_FeedAssigned_AllRead": "You're all caught up.",//Original:"you've all browsed."
   "title_FeedAssigned_": "Gathering",
   "title_Chain_Shareds_": "Contributions",
-  "title_BelongSet_": "Your Corners",
-  "guiding_FeedAssigned_noneAssigned": "Not yet any new unit had been contributed to your corners.",
+  "title_BelongSet_": "Belonged Corners",
+  "guiding_FeedAssigned_noneAssigned": "There's no new unit contributed to your corners.",//unit???Original:"Not yet any new unit had been contributed to your corners."
   "guiding_FeedAssigned_noneAssigned_justSubmit": "Good to have your contrubution.",
   "guiding_FeedAssigned_noneAssigned_norContri": "It could be you contributing somthing for your corners!",
 
   "title_Chain_Shareds_": "Contributions",
   "message_Chain_noShareds": "You'll see the list of your contributions here.",
-  "message_Chain_noSharedsCourage": "Trying submitting an observation about your corners.",
+  "message_Chain_noSharedsCourage": "Try to submit an observation of your corners.",//Original:"Trying submitting an observation about your corners."
   "message_Chain_byChainInfo": [
     "Contribution ",
     "Responded by ",
@@ -117,15 +117,15 @@ const i18nUIString_EN = {
     "Respond to Yours ",
     "succesfully "],
   "message_Chain_toNodes": ["to ", " and several"],
-  "guidingBelongs_EmptyMap": "Set your corner, and taking a look for the distribution of your fellows.",
+  "guidingBelongs_EmptyMap": "Set your corner, and take a look for the distribution of your fellows.", //Changed "taking" to "take"
   "descript_BelongTypeInteract": ["going to renew your ", " ?"],
   "category__Belong_usersCount": ["with ", "users", "way to "],
   "link_BelongsMap_Nav": ["Fellows", "from Home", "same Residence"],
   "messageBelongChoiceinBool": ["site ", "  as your  ", "  place?"],
   "descript_BelongSet_SearchBytType": ["Search one fit your "],
   "descript_AroundIndex_footer": "You've reached the edge of Cornerth.!",
-  'descript_AroundIndex_footer_BelongHint': "start browsing by your current stay or homeland!",
-  "guiding_AroundIndex_Share": "how's your corners",
+  'descript_AroundIndex_footer_BelongHint': "Start browsing by your current stay or homeland!", //Changed "start" to "Start"
+  "guiding_AroundIndex_Share": "Your corners", //Original:"how's your corners"
 
   "descript_Nail_noMark": "(expand image...)"
 };
