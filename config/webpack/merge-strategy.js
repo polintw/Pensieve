663d64f1c2f8f10d727629624b4f8f932380faab
polintw <21116945+polintw@users.noreply.github.com>
2020-06-19 14:22:38 +0800

    fix, rm hotmodule after testing, add script dev-server to package.

--- config/webpack/merge-strategy.js
+++
@@ -1,8 +0,0 @@
-const webpackMerge = require('webpack-merge');
-
-module.exports = webpackMerge.strategy(
-  {
-    'module.rules': 'append',
-    plugins: 'append',
-  },
-);
--- config/webpack/webpack.dev.conf.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('./webpack.base.conf.js');
-const merge = require('./merge-strategy');
-const webpack = require('webpack');
-
-module.exports = merge(baseConfig, {
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ]
-});
