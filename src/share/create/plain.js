14ee53f437970cdc0ab8f0a05bac5bc69343f0f0
polintw <21116945+polintw@users.noreply.github.com>
2020-08-01 14:21:57 +0800

    feat: in api /share, add author validation in main, rename plain to /create, new statics/inspired.

--- src/share/create/plain.js
+++ src/share/create/plain.js
@@ -339,7 +339,7 @@ async function shareHandler_POST(req, res){
 }
 
 execute.post('/', function(req, res){
-  if(process.env.NODE_ENV == 'development') winston.verbose('POST: /share');
+  if(process.env.NODE_ENV == 'development') winston.verbose('POST: /share/create');
   shareHandler_POST(req, res);
 })
 
--- src/share/main.js
+++ src/share/main.js
@@ -1,17 +1,62 @@
 const express = require('express');
 const main = express.Router();
+const winston = require('../../config/winston.js');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const _DB_units = require('../../db/models/index').units;
+const {
+  _handle_ErrCatched,
+  forbbidenError,
+  internalError,
+  validationError
+} = require('../utils/reserrHandler.js');
 
 const editExecutive = require('./editing.js');
 const eraseExecutive = require('./erase.js');
-const staticsExecutive = require('./statics.js');
-const plainExecutive = require('./plain/plain.js');
+const staticsExecutive = require('./statics/execute.js');
+const createExecutive = require('./create/plain.js');
 const accumulatedExecutive = require('./accumulated.js');
 
 main.use('/accumulated', accumulatedExecutive)
 
+main.use('/create', createExecutive)
+
 main.param("id", (req, res, next, id)=>{
   req.reqUnitId = id;
   next();
+})
+
+// only author could edit/erase/get his own shared, examine here for all secondary api
+main.use('/', function(req, res, next){
+  const userId = req.extra.tokenUserId;
+  const exposedId = req.reqUnitId;
+
+  async function validation(){
+    try{
+      // first, select & check the Unit by exposedId
+      let unitOrigin = await _DB_units.findOne({
+        where: {exposedId: exposedId}
+      });
+      if( !unitOrigin ){ // if unitOrigin = null
+        throw new validationError(" from middleware in /share, invalid exposedId from user's req.", 325);
+        return;
+      };
+      // check if the user is author
+      let authorId = unitOrigin.id_author;
+      if(authorId != userId){ // user are not author
+        throw new forbbidenError(" from middleware /share, user: "+ userId+ ", trying approach unit not belong to him/her: "+unitOrigin.id+".", 39);
+        return;
+      };
+      // if everuthing were fine
+      next();
+    }
+    catch(error){
+      _handle_ErrCatched(error, req, res);
+      return;
+    }
+  };
+
+  validation();
 })
 
 main.use('/:id/editing', editExecutive)
@@ -19,7 +64,5 @@ main.use('/:id/editing', editExecutive)
 main.use('/:id/erase', eraseExecutive)
 
 main.use('/:id/statics', staticsExecutive)
-
-main.use('/', plainExecutive)
 
 module.exports = main;
