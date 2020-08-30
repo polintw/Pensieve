20134922420e1ff6cfeb150283acd18ed78d683a
polintw <21116945+polintw@users.noreply.github.com>
2020-08-30 12:47:00 +0800

    chore, edit the text content of the announcement.

--- app/Within/partSign/Announcement.jsx
+++ app/Within/partSign/Announcement.jsx
@@ -99,12 +99,13 @@ class Announcement extends React.Component {
 const toNewsCCServer_title = "| To News as Malicious Domain"
 const toNewsCCServer_content = [
   "Thank you for visiting our platform. I am the main developer and administrator of this site, cornerth.com.",
-  "This announcment is because, as you may heard, our domain was rencently reported unsafe by Ministry of Justice Investigation Bureau, Taiwan, as our domain was used in a series of cyber attack by 2 hacking groups as a C&C server. As soon as we became aware of this situation, we took immediate actions to manage and respond to the issue, including but not limit to suspend our service and investigate any unauthorized access or process across all of our device.",
-  "We nofitified and now cooperated with the related authority to help them clarifying the related cyber campaigns and the way our domain was used by the hacking groups. As the main developer, I personally also engaged several expert for advice from cyber security, fixing any possible weak point among our service.",
+  "This announcment is because, as you may heard, our domain was rencently reported unsafe by Ministry of Justice Investigation Bureau, Taiwan, as our domain was used in a series of cyber attack by 2 hacking groups as a C&C server, which means they use our IP address to hide their real location.",
+  "As soon as we became aware of this situation, we took immediate actions to manage and respond to the issue, including but not limit to suspend our service and investigate any unauthorized access or process across all of our device. We nofitified and now cooperated with the related authority to help them clarifying the cyber campaigns and the way our domain was used by the hacking groups. As the main developer, I personally also engaged several expert for advice from cyber security, fixing any possible weak point among our service.",
   "As our current investigation, there is no evidence that personal information of any nature has been accessed or misused. But we decided to suspend the service for a week before further steps about security were well established.",
   "We are really sorry for what has happened.",
-  "We are a small team base in Taiwan, working on this project since last year. As a start-up project, Cornerth is a platform aim to give people an easy way to share their observation about their homatown or current residence, giving people insight from their aspect. We work on it in believe that the people could learn more even just from their closet environment.",
-  "Once again, we sincerely apologise for this oversight. We do take the safety and security of any user very seriously and will continue to take every action to protect it against any future attacks.",
+  "We are a small team base in Taiwan, working on this project since last year. As a start-up project, Cornerth is a platform aim to give people an easy way to share their observation about their hometown or current residence, giving insight from their very own aspect. We work on it in believe that the people could learn more even just from their closet surrounding.",
+  "Once again, we sincerely apologise for this oversight. We would try our best to prevent any future cyber event like this kind before we put our service back.",
+  "At that moment, welcome you visit again and discover more from people's vision."
 ];
 const toNewsCCServer_Sign = {
   greet:　"Yours sincerely,",
