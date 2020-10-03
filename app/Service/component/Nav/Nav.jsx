fc74fa2b21251110e8929f6cf2545b9232e242ee
polintw <21116945+polintw@users.noreply.github.com>
2020-10-03 13:47:57 +0800

    feat: add mouse effect to Nav of Service and style it.

--- app/Service/component/Nav/Nav.jsx
+++ app/Service/component/Nav/Nav.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
   Link,
-  Redirect,
-  Route,
   withRouter
 } from 'react-router-dom';
 import {connect} from "react-redux";
@@ -11,13 +9,6 @@ import styles from "./styles.module.css";
 import AccountPalette from '../../../Components/AccountPalette.jsx';
 import SvgLogo from '../../../Components/Svg/SvgLogo.jsx';
 
-const styleMiddle = {
-  spanNav: {
-    boxSizing: 'border-box',
-    cursor: 'pointer'
-  },
-}
-
 class Nav extends React.Component {
   constructor(props){
     super(props);
@@ -25,23 +16,12 @@ class Nav extends React.Component {
       mouseOn: ''
     };
     this.refScroll = React.createRef();
+    this._render_serviceNav = this._render_serviceNav.bind(this);
     this._handleEnter_Link = this._handleEnter_Link.bind(this);
     this._handleLeave_Link = this._handleLeave_Link.bind(this);
     this.style={
 
     }
-  }
-
-  _handleEnter_Link(e){
-    this.setState({
-      mouseOn: e.currentTarget.attributes.method.value
-    })
-  }
-
-  _handleLeave_Link(e){
-    this.setState({
-      mouseOn: ''
-    })
   }
 
   componentDidUpdate(prevProps, prevState, snapshot){
@@ -56,17 +36,65 @@ class Nav extends React.Component {
 
   }
 
+  _render_serviceNav(){
+    const navItems = ["about", 'terms', "privacy", "contact"];
+    const currentPath = window.location.pathname;
+    
+    let navDOM = navItems.map((text, index)=> {
+      // make the 1st char in text to cap
+      let upperChar = text.charAt(0).toUpperCase();
+      let restChar = text.slice(1);
+      let displayText = upperChar + restChar;
+      // check if on path 
+      let pathify = currentPath.includes(text);
+      
+      return (
+        <Link
+          key={"key_serviceNav_"+index}
+          to= {"/" + text}
+          topath={text}
+          className={classnames(
+            'plainLinkButton', styles.boxLink, styles.boxNavLink,
+            {[styles.boxNavLinkMouse]: (this.state.mouseOn == text )}
+          )}
+          onMouseEnter={this._handleEnter_Link}
+          onMouseLeave={this._handleLeave_Link}>
+          <span
+            className={classnames(
+              {
+                ["weightBold"]: (pathify),
+                ["colorStandard"]: (pathify),
+                ["colorEditBlack"]: (this.state.mouseOn == text && !pathify),
+              }
+            )}>
+            {displayText}
+          </span>        
+        </Link>
+      )
+    })
+    
+    return navDOM;
+  }
+
   render(){
     return (
       <div
         className={classnames(styles.comAboutNav, styles.fontNav)}>
-        <div
-          className={classnames(styles.comNavOption)}>
+        <div>
           { // if under a valid token
             (this.props.tokenStatus == 'verified') ? (
               <a
                 href="/"
-                className={classnames('plainLinkButton', 'colorDescripBlack')}>
+                topath={"index"}
+                className={classnames(
+                  'plainLinkButton', 'colorDescripBlack', styles.boxLink,
+                  { 
+                    [styles.linkMouse]: (this.state.mouseOn == 'index'),
+                    [styles.linkNoMouse]: (this.state.mouseOn != 'index')
+                  }
+                )}
+                onMouseEnter={this._handleEnter_Link}
+                onMouseLeave={this._handleLeave_Link}>
                 <div
                   className={classnames(styles.smallDisplayBox)}
                   style={{height:"20px"}}>
@@ -85,47 +113,38 @@ class Nav extends React.Component {
             ):(
               <a
                 href="/"
+                topath={"index"}
                 className={classnames(
-                  'plainLinkButton', 'fontSubtitle', 'colorDescripBlack')}>
+                  'plainLinkButton', 'colorDescripBlack', 'fontSubtitle', styles.boxLink,
+                  { 
+                    [styles.linkMouse]: (this.state.mouseOn == 'index'),
+                    [styles.linkNoMouse]: (this.state.mouseOn != 'index')
+                  }
+                )}
+                onMouseEnter={this._handleEnter_Link}
+                onMouseLeave={this._handleLeave_Link}>
                 {this.props.i18nUIString.catalog['submit_nav_Signin']}
               </a>
             )
           }
         </div>
         <div>
-          <Link
-            to="/terms"
-            method="terms"
-            className={classnames('plainLinkButton', styles.boxLink)}
-            onMouseEnter={this._handleEnter_Link}
-            onMouseLeave={this._handleLeave_Link}>
-            {
-              (this.state.mouseOn=='terms') &&
-              <span style={{
-                  width: '80%', position: 'absolute', bottom: '-11%', left: '10%',
-                  borderBottom: 'solid 1px rgb(64, 133, 160)'
-                }}/>
-              }
-              <span>{"Terms"}</span>
-            </Link>
-            <Link
-              to="/privacy"
-              method="privacy"
-              className={classnames('plainLinkButton', styles.boxLink)}
-              onMouseEnter={this._handleEnter_Link}
-              onMouseLeave={this._handleLeave_Link}>
-              {
-                (this.state.mouseOn=='privacy') &&
-                <span style={{
-                    width: '80%', position: 'absolute', bottom: '-11%', left: '10%',
-                    borderBottom: 'solid 1px rgb(64, 133, 160)'
-                  }}/>
-                }
-                <span>{"Privacy"}</span>
-              </Link>
+          {this._render_serviceNav()}
         </div>
       </div>
     )
+  }
+
+  _handleEnter_Link(e) {
+    this.setState({
+      mouseOn: e.currentTarget.attributes.topath.value
+    })
+  }
+
+  _handleLeave_Link(e) {
+    this.setState({
+      mouseOn: ''
+    })
   }
 }
 
